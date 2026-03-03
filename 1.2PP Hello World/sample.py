48500cb6eda31264a9bf1fdf3d19aa2e9b410505
neillerin99 <lerin113@gmail.com>
2026-03-03 11:50:36 +1100

    saple.py input and output from lecture

--- 1.2PP Hello World/sample.py
+++ 1.2PP Hello World/sample.py
@@ -16,10 +16,14 @@ def main():
     average:int = (5+7)/2
     greet:str = 'Hello world'
     sample_bool: bool = True
+    name:str
+    name = input("Enter your name:")
+    # f is a way we can format and we can insert values
+    print(f"My name is {name} and I am {a} years old")
     # age =  int(input("Enter age:"))
     # total = age + a
     # print("Your age is: ", total)
-    print(math.ceil(2.2224))
+    # print(math.ceil(2.2224))
     # print(turtle.forward(5))
     # print(ceil(2.2224))
     # print(average)
