b000eb9f5ef9efcd60b1edcdc2e3dad5cfd9e8d8
Neil <lerin113@gmail.com>
2026-03-03 14:33:57 +1100

    feat: adding of comments for hello_world.py
    -changing file directory structure

--- 1.2PP Hello World/hello_world.py
+++ 1.2PP Hello World/hello_world.py
@@ -1,10 +1,13 @@
 """
-1.2PP Hello World
+1.2PP Hello World: A program that displays multiple lines of text
 """
 
 __author__ = "NEIL EDRIANE LERIN"
 
 def main():
+    """
+    Printing multiple lines of text
+    """
     print("Hi my name is Neil!")
     print("I am a new student in UTAS.")
     print("And I am very excited for my MITS journey!")
