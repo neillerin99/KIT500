6a6011f141f0ea9e6cb3fc283df1eb0214399f06
neillerin99 <lerin113@gmail.com>
2026-03-07 21:50:36 +1100

    2.1PP Turtle Graphics final version

--- 2.1PP Turtle Graphics/initials.py
+++ 2.1PP Turtle Graphics/initials.py
@@ -1,7 +1,7 @@
 """
 KIT101 2.1PP Turtle Graphics
 
-Turtle Graphics task to draw the author's initials.
+This program draws my initials (NL) using turtle graphics.
 """
 
 __author__ = "NEIL EDRIANE LERIN"
@@ -10,33 +10,28 @@ import turtle as t
 
 
 def main():
-    # Change turtle speed if desired
-    # (1=slowest .. 10=fastest | 0=no animation)
     t.speed(3)
+    t.color('red')  # set pointer color to red
 
-    # set initial color to red
-    t.color('red')
-
-    # Rotate pointer by full 180 and move forward to center the initials
+    # Code block to move the starting position to the left, to center the initials upon writing
     t.penup()
     t.left(180)
     t.forward(100)
     t.pendown()
     
     # code block to draw the initial N
-    t.right(90) # recenter the pointer to point up
-    t.forward(150) # move the pointer 150 forward
-    t.right(150) # reposition the pointer header to right 150 degrees
-    t.forward(170) # move the pointer 170 forward
-    t.left(150) # reposition the pointer header to left to revert back to up pointing position
-    t.forward(150) # move the pointer 150 forward
+    t.right(90) 
+    t.forward(150) 
+    t.right(150) 
+    t.forward(170) 
+    t.left(150) 
+    t.forward(150) 
 
     # code block to create a space between letters and reposition the header
     t.penup()
     t.right(90)
     t.forward(20)
     t.right(90)
-    t.color('blue') # set pointer color to blue
     t.pendown()
 
     # code block to draw the Initial L
@@ -44,11 +39,8 @@ def main():
     t.left(90)
     t.forward(90)
 
-    # hide the turtle head
-    t.hideturtle()
-
-    # Avoid closing the window automatically
-    t.mainloop()
+    t.hideturtle() # hide the turtle head
+    t.mainloop()  # Avoid closing the window automatically
 
 
 if __name__ == "__main__":
