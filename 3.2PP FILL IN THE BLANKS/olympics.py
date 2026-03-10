bb23355812e9fecf062996e378a2502bf006d00e
Neil <90372343+neillerin99@users.noreply.github.com>
2026-03-10 15:49:38 +1100

    feat: 3.2 PP Fill in the blanks
    -added input statements for olympics.py

--- 3.2PP FILL IN THE BLANKS/olympics.py
+++ 3.2PP FILL IN THE BLANKS/olympics.py
@@ -6,12 +6,29 @@ __author__ = "NEIL EDRIANE LERIN"
 
 
 def main():
-    print("Paris 2024 Medal Tally")
+    first_place: str # country variable with the most gold medals
+    capital_city: str # capital city of the country in first place
+    second_place: str  # country with the second most gold medals currently
+    gold_medals_1: int # number of gold medals won by the country in first place
+    gold_medals_2: int # number of gold medals won by the country in second place
+    margin: int #differnce between gold medal counts
+
+    print("PARIS OLYMPICS DAILS GOLD TALLY")
     print()
 
-    first_place: str = input("Enter the name of the country in first place: ")
-    gold_medals_1: int = int(input("How many gold medals has the first-placed country won? "))
+    first_place = input("Enter the name of the country in first place: ")
+    capital_city = input("Enter the captial city of the first place country: ")
 
+    second_place = input("Enter the name of the country in second place: ")
+    
+    gold_medals_1 = int(input("How many gold medals has the first-placed country won? "))
+    gold_medals_2 = int(input("How many gold medals has the second-placed country won? "))
+
+    margin = gold_medals_1 - gold_medals_2
+
+    print(f"{first_place} currently tops the 2024 Paris Olmypics' medal tally with {gold_medals_1} gold.")
+    print(f"Citizens of {capital_city} are overjoyed at beating arch-rivals {second_place}.")
+    print(f"Their lead of {margin} gold looks secure with only competitive programming left.")
 
 if __name__ == "__main__":
     main()
