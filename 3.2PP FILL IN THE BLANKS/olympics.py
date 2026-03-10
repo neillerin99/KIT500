f22d096a5e6365d19268cdbd143edc8c34f6d436
Neil <90372343+neillerin99@users.noreply.github.com>
2026-03-10 15:32:19 +1100

    feat: 3.2PP Fill in the blanks
    -added olympics.py

---
+++ 3.2PP FILL IN THE BLANKS/olympics.py
@@ -0,0 +1,17 @@
+"""
+3.2PP Fill in the Blanks: Paris Olympics
+"""
+
+__author__ = "NEIL EDRIANE LERIN"
+
+
+def main():
+    print("Paris 2024 Medal Tally")
+    print()
+
+    first_place: str = input("Enter the name of the country in first place: ")
+    gold_medals_1: int = int(input("How many gold medals has the first-placed country won? "))
+
+
+if __name__ == "__main__":
+    main()
